dc32cb8864821a4675155b9b8f6291ed49cb56d9
JuanSuppicich <suppicichjuan@gmail.com>
2021-08-12 16:38:26 -0300

    Add Spring Initializr

---
+++ src/test/java/com/durandsuppicich/danmsdelivery/DanMsDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.durandsuppicich.danmsdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanMsDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
