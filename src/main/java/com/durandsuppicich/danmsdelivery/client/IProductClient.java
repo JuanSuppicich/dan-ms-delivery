88c7829ce1ee9686c90b025e7839ecf6b2f64ad8
JuanSuppicich <suppicichjuan@gmail.com>
2021-08-20 16:02:32 -0300

    Add missing annotation

--- src/main/java/com/durandsuppicich/danmsdelivery/client/IProductClient.java
+++ src/main/java/com/durandsuppicich/danmsdelivery/client/IProductClient.java
@@ -2,6 +2,7 @@ package com.durandsuppicich.danmsdelivery.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -9,6 +10,6 @@ import java.util.List;
 public interface IProductClient {
 
     @GetMapping(value = "/api/products/all-in")
-    List<ProductDto> getAllByIds(List<Integer> ids);
+    List<ProductDto> getAllByIds(@RequestBody List<Integer> ids);
 
 }
